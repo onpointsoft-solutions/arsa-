078029dadf359be880b2250374edaa4df5e6719c
OnpointSoftwares <vincentbettoh@gmail.com>
2026-08-21 13:34:11 +0300

    fix: use allowedHosts array, add static site cache headers

--- vite.config.ts
+++ vite.config.ts
@@ -33,13 +33,13 @@ export default defineConfig(({ mode }) => {
       host: '0.0.0.0',
       port: parseInt(process.env.PORT || '8443'),
       strictPort: true,
-      allowedHosts: 'all',
+      allowedHosts: ['all'],
       watch: { ignored: ['**/.figma/**'] },
     },
     preview: {
       host: '0.0.0.0',
       port: parseInt(process.env.PORT || '8443'),
-      allowedHosts: 'all',
+      allowedHosts: ['all'],
     },
   }
 })
